4972c15e9b80224b9d1cf9f9996e9a4980fa5351
kmh339 <kmh339@gmail.com>
2019-11-27 02:28:36 +0900

    Initial commit

---
+++ app/src/main/java/org/androidtown/megacody/MainActivity.java
@@ -0,0 +1,27 @@
+package org.androidtown.megacody;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity{
+    Button cameraOnButton;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        cameraOnButton = findViewById(R.id.cameraButton);
+        cameraOnButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(MainActivity.this, LaunchActivity.class));
+            }
+        });
+
+    }
+
+}
