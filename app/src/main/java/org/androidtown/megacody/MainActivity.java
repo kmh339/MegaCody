6d46d06ef3f2f3eb8276b04634479e09490aab6b
kmh339 <kmh339@gmail.com>
2019-11-27 04:11:45 +0900

    Camera, gallery

--- app/src/main/java/org/androidtown/megacody/MainActivity.java
+++ app/src/main/java/org/androidtown/megacody/MainActivity.java
@@ -18,9 +18,10 @@ public class MainActivity extends AppCompatActivity{
         cameraOnButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                startActivity(new Intent(MainActivity.this, LaunchActivity.class));
+                startActivity(new Intent(MainActivity.this, CameraActivity.class));
             }
         });
+
 
     }
 
